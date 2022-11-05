000565d090229326bd2f27d5b0953d3aa34c52f7
obasoro <obasorokunle@gmail.com>
2022-11-05 05:00:50 -0500

    update

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -11,11 +11,7 @@ class App extends Component {
           <h1 className="App-title">HNGi9 DevOps Stage2 Task</h1>
         </header>
         <p className="App-intro">
-<<<<<<< HEAD
           This task was submitted by <b>Olakunle Obasoro</b>
-=======
-          This task was submitted by <b>Obasoro Olakunle</b>
->>>>>>> 030accaa06abaa0c1ccaff0c4b91d228d3dd6b1e
         </p>
       </div>
     );
